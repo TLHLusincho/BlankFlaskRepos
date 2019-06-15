f91d199c4ca9c62c498231d4a295b8d0c83519d2
lusine.torosyan <lusine.torosyan@helpsystems.com>
2019-06-15 16:19:40 +0400

    Commented the part which running code

--- FirstFlaskExample/FirstFlaskExample/app.py
+++ FirstFlaskExample/FirstFlaskExample/app.py
@@ -16,10 +16,10 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
+   import os
+   HOST = os.environ.get('SERVER_HOST', 'localhost')
+   try:
+       PORT = int(os.environ.get('SERVER_PORT', '5555'))
+   except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    # app.run(HOST, PORT)
